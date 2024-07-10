bdba474181fbe13e97657382de02038f59db3f76
Satrio Kamaludin <satriokamaludin@gmail.com>
2024-07-11 05:41:55 +0700

    task commit

--- src/main/java/com/tujuhsembilan/example/controller/NoteController.java
+++ src/main/java/com/tujuhsembilan/example/controller/NoteController.java
@@ -5,6 +5,8 @@ import java.util.stream.Collectors;
 import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,19 +29,25 @@ public class NoteController {
   private final ModelMapper mdlMap;
 
   @GetMapping
-  public ResponseEntity<?> getNotes() {
+  public ResponseEntity<?> getNotes(Authentication auth) {
     return ResponseEntity
-        .ok(repo.findAll()
+        .ok(repo.findByUsername(getUsername(auth))
             .stream()
             .map(o -> mdlMap.map(o, NoteDto.class))
             .collect(Collectors.toSet()));
   }
 
   @PostMapping
-  public ResponseEntity<?> saveNote(@RequestBody NoteDto body) {
+  public ResponseEntity<?> saveNote(@RequestBody NoteDto body, Authentication auth) {
     var newNote = mdlMap.map(body, Note.class);
+    newNote.setUsername(getUsername(auth));
     newNote = repo.save(newNote);
     return ResponseEntity.status(HttpStatus.CREATED).body(newNote);
   }
 
+  private String getUsername(Authentication auth) {
+    Jwt jwt = (Jwt) auth.getPrincipal();
+    String username = jwt.getSubject();
+    return username;
+  }
 }
